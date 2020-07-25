c23b871640417f7e7a4a95acb1fdd9a04436c68e
Nicolas Forero <ns.forero10@gmail.com>
2020-07-25 00:05:15 -0500

    [Fix] betty 5 task

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - creates a binary tree node
+ * binary_tree_is_root - creates a binary tree node
  * @node: the node to check
  * Return: 1 if node is a root, otherwise 0
  */
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node || node->parent)
 		return (0);
